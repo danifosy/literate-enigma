b9fc19fe6700b3667ff40096e4680ec30f03cdc5
danifosy <mail@daniha.de>
2021-08-03 14:16:45 +0200

    🔨 Add createElement.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,42 @@
 import './style.css';
+import './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const mainElement = document.createElement('main');
+const mainElement = createElement('main', {
+  childElements: [createElement('h1', { innertext: 'Rick and Morty' })],
+  createElement,
+});
+
+/* const mainElement = document.createElement('main');
+mainElement.className = 'container';
 
 const title = document.createElement('h1');
-title.innerText = 'Rick and Morty App';
+title.innerText = 'Rick and Morty';
 
 const subTitle = document.createElement('h2');
-subTitle.innerText =
-  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+subTitle.innerText = 'Lorem ipsum dolor sit amet,';
 
 const characterSearch = document.createElement('input');
-characterSearch.placeholder = 'Deep dive into the world of Rick and Morty...';
+characterSearch.placeholder = 'Dive into the world of Rick and Morty...';
+
+const characterCards = document.createElement('div');
+characterCards.className = 'character-cards';
+
+const characterCardArticle = document.createElement('article');
+characterCardArticle.className = 'cc-article';
+
+const characterCardHeader = document.createElement('h3');
+characterCardHeader.className = 'cc-header';
+
+const characterCardImg = document.createElement('img');
+characterCardImg.className = 'cc-img';
+
+characterCardArticle.append(characterCardHeader, characterCardImg);
+characterCards.append(characterCardArticle);*/
 
 if (app !== null) {
   app.append(mainElement);
 }
 
-mainElement.append(title, subTitle, characterSearch);
+mainElement.append(title, subTitle, characterSearch, characterCards);
