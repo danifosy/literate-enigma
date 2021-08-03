15462fafff855fa81a5368d4115c83f6c526d4ae
danifosy <mail@daniha.de>
2021-08-03 20:27:10 +0200

    Add second character card

--- src/components/character/character.ts
+++ src/components/character/character.ts
@@ -34,3 +34,36 @@ export function characterCards(): HTMLElement {
     ],
   });
 }
+export function characterCards1(): HTMLElement {
+  return createElement('div', {
+    className: 'cc-grid',
+    childElements: [
+      createElement('article', {
+        className: 'cc-article',
+        childElements: [
+          createElement('img', {
+            src: 'https://rickandmortyapi.com/api/character/avatar/102.jpeg',
+            className: 'cc-img',
+          }),
+          createElement('div', {
+            className: 'cc-description',
+            childElements: [
+              createElement('h3', {
+                innerText: 'Donna Gueterman',
+                className: 'cc-header',
+              }),
+              createElement('p', {
+                innerText: 'Dead - Robot',
+                className: 'cc-status',
+              }),
+              createElement('p', { innerText: 'Planet Squanch' }),
+              createElement('p', {
+                innerText: 'The Wedding Squanchers',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './style.css';
 import { createElement } from './utils/createElement';
-import { characterCards } from './components/character/character';
+import {
+  characterCards,
+  characterCards1,
+} from './components/character/character';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
@@ -23,36 +26,9 @@ const mainElement = createElement('main', {
   ],
 });
 
-/* const mainElement = document.createElement('main');
-mainElement.className = 'container';
-
-const title = document.createElement('h1');
-title.innerText = 'Rick and Morty';
-
-const subTitle = document.createElement('h2');
-subTitle.innerText = 'Lorem ipsum dolor sit amet,';
-
-const characterSearch = document.createElement('input');
-characterSearch.placeholder = 'Dive into the world of Rick and Morty...';
-
-const characterCards = document.createElement('div');
-characterCards.className = 'character-cards';
-
-const characterCardArticle = document.createElement('article');
-characterCardArticle.className = 'cc-article';
-
-const characterCardHeader = document.createElement('h3');
-characterCardHeader.className = 'cc-header';
-
-const characterCardImg = document.createElement('img');
-characterCardImg.className = 'cc-img';
-
-characterCardArticle.append(characterCardHeader, characterCardImg);
-characterCards.append(characterCardArticle);*/
-
 if (app !== null) {
   app.append(mainElement);
-  mainElement.append(characterCards());
+  mainElement.append(characterCards(), characterCards1());
 }
 
 //mainElement.append(title, subTitle, characterSearch, characterCards);
