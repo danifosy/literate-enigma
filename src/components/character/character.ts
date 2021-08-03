0a844b0b5dccd4d815e69b75d1699a4b69b62362
danifosy <mail@daniha.de>
2021-08-03 20:07:32 +0200

    Add grid to character card

--- src/components/character/character.ts
+++ src/components/character/character.ts
@@ -12,29 +12,22 @@ export function characterCards(): HTMLElement {
             src: 'https://rickandmortyapi.com/api/character/avatar/27.jpeg',
             className: 'cc-img',
           }),
-          createElement('h3', {
-            innerText: 'Artist Morty',
-            className: 'cc-header',
-          }),
-          createElement('p', {
-            innerText: 'Alive - Human',
-            className: 'cc-status',
-          }),
-        ],
-      }),
-      createElement('div', {
-        className: 'cc-location',
-        childElements: [
-          createElement('p', { innerText: 'Last known location:' }),
-          createElement('p', { innerText: 'Citadel of Ricks' }),
-        ],
-      }),
-      createElement('div', {
-        className: 'cc-appearance',
-        childElements: [
-          createElement('p', { innerText: 'Frist seen in: ' }),
-          createElement('p', {
-            innerText: 'Close Rick-counters of the Rick Kind',
+          createElement('div', {
+            className: 'cc-description',
+            childElements: [
+              createElement('h3', {
+                innerText: 'Artist Morty',
+                className: 'cc-header',
+              }),
+              createElement('p', {
+                innerText: 'Alive - Human',
+                className: 'cc-status',
+              }),
+              createElement('p', { innerText: 'Citadel of Ricks' }),
+              createElement('p', {
+                innerText: 'Close Rick-counters of the Rick Kind',
+              }),
+            ],
           }),
         ],
       }),
