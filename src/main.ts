b422fe21fde66344b694709e640fd35e31d1f245
danifosy <mail@daniha.de>
2021-08-03 12:18:45 +0200

    Add html node elements

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
+const mainElement = document.createElement('main');
+
+const title = document.createElement('h1');
+title.innerText = 'Rick and Morty App';
+
+const subTitle = document.createElement('h2');
+subTitle.innerText =
+  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+
+const characterSearch = document.createElement('input');
+characterSearch.placeholder = 'Deep dive into the world of Rick and Morty...';
+
 if (app !== null) {
-  app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+  app.append(mainElement);
 }
+
+mainElement.append(title, subTitle, characterSearch);
