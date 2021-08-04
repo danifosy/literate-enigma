f7501b50c5fe54b3b6dba9462b2958e2e441d081
danifosy <mail@daniha.de>
2021-08-04 16:22:35 +0200

    Add characters array and map over createCharacterCards function

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import { createCharacterCards } from './components/character/character';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const character: Character = {
-  thumbnail: 'https://rickandmortyapi.com/api/character/avatar/133.jpeg',
-  name: 'Garblovian',
-  status: 'Alive - Alien',
-  origin: 'unknown location',
-};
-
+const characters: Character[] = [
+  {
+    thumbnail: 'https://rickandmortyapi.com/api/character/avatar/133.jpeg',
+    name: 'Garblovian',
+    status: 'Alive - Alien',
+    origin: 'unknown location',
+  },
+];
 const mainElement = createElement('main', {
   className: 'container',
   childElements: [
@@ -25,12 +26,12 @@ const mainElement = createElement('main', {
       placeholder: 'Dive into the world of Rick and Morty...',
     }),
     createElement('h2', { innerText: 'Your character of the day is:' }),
+    ...characters.map((character) => createCharacterCards(character)),
   ],
 });
 
 if (app !== null) {
   app.append(mainElement);
-  mainElement.append(createCharacterCards(character));
 }
 
 //mainElement.append(title, subTitle, characterSearch, characterCards);
